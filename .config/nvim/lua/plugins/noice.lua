7ecfad08c959c12025fa878ee8a9e51d33ee4665
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2026-03-14 12:01:20 -0300

    fix(nvim): add split keymaps and fix noice.nvim UI error with gopls

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/noice.nvim",
+  opts = function(_, opts)
+    opts.routes = opts.routes or {}
+    table.insert(opts.routes, {
+      filter = {
+        event = "lsp",
+        kind = "progress",
+        cond = function(message)
+          local client = vim.tbl_get(message.opts, "progress", "client")
+          return client == "gopls"
+        end,
+      },
+      opts = { skip = true },
+    })
+  end,
+}
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,6 +37,8 @@ keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
+keymap.set("n", "<Leader>\\", ":vsplit<CR>", opts)
+keymap.set("n", "<Leader>-", ":split<CR>", opts)
 --keymap.set("n", "ss", ":split", opts)
 --keymap.set("n", "sv", ":vsplit", opts)
 
