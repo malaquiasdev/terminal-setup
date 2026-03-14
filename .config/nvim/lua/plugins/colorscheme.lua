a5601988e9704beca63eabb3c68eea6bce90eadc
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2026-03-14 13:38:48 -0300

    feat(theme): switch from solarized to tokyo-night (nvim/iterm/tmux)

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
