3294ce41ce921bb0abd0cc51c669063f40e461c9
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2026-03-14 12:28:30 -0300

    feat(nvim): restore custom split keymaps after migration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -46,6 +46,10 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Split window
+keymap.set("n", "<Leader>\\", ":vsplit<CR>", opts)
+keymap.set("n", "<Leader>-", ":split<CR>", opts)
+
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
