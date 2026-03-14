fa7953a5b8c8c4ee49098330f8db283ec479922e
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2026-03-14 13:54:24 -0300

    fix(nvim): use absolute line numbers and fixed signcolumn for UI stability

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,6 +4,8 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes"
 
 vim.opt.title = true
 vim.opt.autoindent = true
